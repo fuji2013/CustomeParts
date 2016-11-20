bc4924c6fa1ed650ab21ad67c7a662869fdb9604
hf <fuhi1983@gmail.com>
2016-11-20 19:33:07 +0900

    new project

---
+++ CustomParts/CustomParts/CustomParts.h
@@ -0,0 +1,19 @@
+//
+//  CustomParts.h
+//  CustomParts
+//
+//  Created by hf on 2016/11/20.
+//  Copyright © 2016年 swift-studying.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomParts.
+FOUNDATION_EXPORT double CustomPartsVersionNumber;
+
+//! Project version string for CustomParts.
+FOUNDATION_EXPORT const unsigned char CustomPartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomParts/PublicHeader.h>
+
+
